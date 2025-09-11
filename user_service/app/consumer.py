f812cceed41fbe9ded031ecc37934d938213a8ba
aditya-sumcircle <aditya@sumcircle.com>
2025-09-11 15:42:39 +0530

    changes

--- user_service/app/consumer.py
+++ user_service/app/consumer.py
@@ -1,5 +1,9 @@
 import pika
 import json
+from . import create_app
+from .models import db , User
+
+app = create_app()
 
 params = pika.URLParameters('amqps://xhpriczr:PHGniONLdwZjj4i4KrXTEvydiE4CSF9m@kebnekaise.lmq.cloudamqp.com/xhpriczr')
 
@@ -10,8 +14,20 @@ def callback(ch , method , properties, body):
     if message.get("action") == "create_user":
         user_data = message["data"]
         print(f"Creating user in DB: {user_data}")
-        # TODO: save to database
-        
+        with app.app_context():
+            try:
+                new_user = User(
+                    id=user_data.get("user_id"),
+                    full_name=user_data.get("full_name"),
+                    email=user_data.get("email")
+                )
+                db.session.add(new_user)
+                db.session.commit()
+                print(f"✅ User {new_user.full_name} added successfully")
+            except Exception as e:
+                db.session.rollback()
+                print(f"❌ Error inserting user: {e}")
+
     ch.basic_ack(delivery_tag=method.delivery_tag)
     
 def start_consuming():
--- user_service/app/run.py
+++ user_service/app/run.py
@@ -1,9 +1,13 @@
+import os
+
 from app import create_app
 
 app = create_app()
 
-# @app.route("/")
 
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URL", "postgresql://postgres:1234@localhost:5432/user_db"
+)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
